09d2aab7d561bfc093d33074f470120a38f7c3ba
T527B <brian.sumner@ucdenver.edu>
2016-01-23 19:26:43 -0700

    Restarted pa0 project due to security flaw

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
  */
 #include <iostream>
 
+const int MY_ID_MODULO_13 = 12;
+
 int main() {
     // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
-
+    std::cout << "Hello, world! My number is " << MY_ID_MODULO_13 << '!' << std::endl;
     return 0;
 }
